23d18495873745509573f9531073f9100111ddfe
tjcouch-sil <tj_couch@sil.org>
2023-07-07 11:18:20 -0500

    Added more explanation for why we have public/assets/index.d.ts

--- public/assets/index.d.ts
+++ public/assets/index.d.ts
@@ -1,1 +1,4 @@
-// This file is here to make tsconfig accept that we want types from `public` without getting angry that we have the `assets` folder that doesn't have or need types
+// This file is here to make tsconfig accept that we want types from `public` without getting angry
+// that we have the `assets` folder that doesn't have or need types.
+// We can remove it if/when we move `paranext-extension-template.d.ts` to a different place and
+// remove `public` from `typeRoots` in `tsconfig.json`
