ef760aebecc8974d6ec561713c322c29e4966f2c
tombogle <tom_bogle@sil.org>
2025-07-29 11:18:02 -0400

    Extended the comment in paranext-extension-template.d.ts to point to the useful section of the wiki page

--- src/types/paranext-extension-template.d.ts
+++ src/types/paranext-extension-template.d.ts
@@ -1,4 +1,5 @@
 declare module 'paranext-extension-template' {
   // Add extension types exposed on the papi for other extensions to use here
-  // More instructions can be found in the README
+  // More instructions can be found in the README and at
+  // https://github.com/paranext/paranext-extension-template/wiki/Extension-Development-'How-To'-Guide#add-a-new-type-of-data
 }
